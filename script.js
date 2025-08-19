e61d687901b0c365c139825be2e613e1eca5c229
Guilherme Bilibio <guilherme.bilibio0505@gmail.com>
2025-08-19 16:12:41 -0300

    improved js

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ function verificationNumber(userNumber, randomNumber) {
     if (userNumber == randomNumber) {
         return "Você acertou!"
     } else {
+        distanceFromNumber(userNumber, randomNumber)
+
         return "Você errou!"
     }
 }
 
-function distanceFromNumber() {
-    // Code here
+function distanceFromNumber(userNumber, randomNumber) {
+    gap = ((userNumber - randomNumber) ** 2) ** 0.5
+
+    if (gap < 3) {
+        return "Morno"
+    } else if (gap < 2) {
+        return "Quente"
+    } else if (gap < 1) {
+        return "Muito quente"
+    }
 }
 
 function game() {
